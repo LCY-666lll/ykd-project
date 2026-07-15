0e6e52f3503969221994db4655639bfc83a942e7
LiChenyang <3898342737@qq.com>
2026-07-15 16:40:02 +0800

    删除hello

--- src/main/java/com/fourth/ykd/zuo/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.fourth.ykd.zuo;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("qsudujashdjkashdajasdsadasdjahjdhasdjkhaskdjhkadhaskjdhskja");
-    }
-}
